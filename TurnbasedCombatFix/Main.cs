bed274cd46bf9d02b32deeefdb8d213a3a31f599
spacehamster <mail.spacehamster@gmail.com>
2020-08-22 09:26:32 +1000

    Added project

---
+++ TurnbasedCombatFix/Main.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace TurnbasedCombatFix
+{
+#if DEBUG
+    [EnableReloading]
+#endif
+    public class Main
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            modEntry?.Logger?.Log(msg);
+        }
+        public static void Error(Exception ex)
+        {
+            modEntry?.Logger?.Error(ex.ToString());
+        }
+        public static void Error(string err)
+        {
+            modEntry?.Logger?.Error(err);
+        }
+        public static bool enabled;
+        public static UnityModManager.ModEntry modEntry;
+        public static Harmony harmony;
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try { 
+                Main.modEntry = modEntry;
+#if DEBUG
+                modEntry.OnUnload = Unload;
+#endif
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+        static bool Unload(UnityModManager.ModEntry modEntry)
+        {
+            harmony.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+    }
+}
---
+++ TurnbasedCombatFix/Patches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Kingmaker.UI._ConsoleUI.CombatStartScreen;
+using Kingmaker.UI.TurnBasedMode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnbasedCombatFix
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(TurnBasedModeUIController), "ShowCombatStartWindow")]
+        static class TurnBasedModeUIController_ShowCombatStartWindow_Patch
+        {
+            static bool Prefix(ref CombatStartWindowVM ___m_CombatStartWindowVM)
+            {
+                if (___m_CombatStartWindowVM != null)
+                {
+                    Main.Log("Skipping show combat window");
+                    ___m_CombatStartWindowVM.Dispose();
+                    ___m_CombatStartWindowVM = null;
+                }
+                else
+                {
+                    Main.Log("CombatStartWindow is null");
+                }
+                return false;
+            }
+        }
+    }
+}
